ae30bd49c0d19d3bd5f6bfecfd29d3579fd376dc
Joe Morris <joem@joe-morriss-macbook-pro.local>
2014-04-24 18:22:37 -0400

    Initial Commit

---
+++ NewWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NewWorld
+//
+//  Created by Joe Morris on 4/24/14.
+//  Copyright (c) 2014 joemdesign, llc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
